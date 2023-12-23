1ece8089b681a90018a0658cc047491ca4431d16
GKastet <127600582+GKastet@users.noreply.github.com>
2023-12-23 10:03:02 +0100

    pages

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Contacts = () => {
+  return (
+    <>
+      <div>Contacts</div>
+      <Link>Home</Link>
+    </>
+  );
+};
+
+export default Contacts;
